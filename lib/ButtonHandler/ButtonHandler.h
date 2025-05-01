d7e358a7173edc16c4bef649c84d29a62af1cf8a
Matt Troha <mjt1701@comcast.net>
2025-05-01 09:02:04 -0400

    updated ButtonHandler now need to merge that with main.cpp

--- lib/ButtonHandler/ButtonHandler.h
+++ lib/ButtonHandler/ButtonHandler.h
@@ -1,35 +1,59 @@
 #pragma once
 #include <Arduino.h>
 
+// class ButtonHandler
+// {
+// public:
+//     ButtonHandler(uint8_t pin, bool activeState = LOW, unsigned long longPress = 2000, unsigned long debounce = 50);
+
+//     void reset();
+//     void update();
+
+//     bool isShortPress();
+//   //  bool isLongPress();       // true after long press and release
+//  //   bool isNewLongPress();    // true the moment long press starts
+//     bool wasLongPressStart(); // Returns true once when long press first detected
+//     bool isHeldLong();        // True as long as the button is held beyond long press duration
+//     bool isPhysicallyReleased() const;
+
+// private:
+//     uint8_t pin;
+//     bool activeState;
+//     unsigned long longPressDuration;
+//     unsigned long debounceDelay;
+
+//     bool shortPressDetected = false;
+//     bool longPressDetected = false;
+//     bool longPressJustStarted = false;
+//  //   bool isPressing = false;
+
+//     unsigned long pressStartTime = 0;
+//     unsigned long lastDebounceTime = 0;
+//     bool lastReading = HIGH; // Default unpressed for pull-up config
+
+//     public:
+//     bool isButtonHeld() const { return isPressing; }
+// };
+
+#include <Arduino.h>
+
 class ButtonHandler
 {
 public:
-    ButtonHandler(uint8_t pin, bool activeState = LOW, unsigned long longPress = 2000, unsigned long debounce = 50);
-
-    void reset();
-    void update();
-
-    bool isShortPress();
-  //  bool isLongPress();       // true after long press and release
- //   bool isNewLongPress();    // true the moment long press starts
-    bool wasLongPressStart(); // Returns true once when long press first detected
-    bool isHeldLong();        // True as long as the button is held beyond long press duration
-    bool isPhysicallyReleased() const;
-    
+  ButtonHandler(uint8_t pin);
+  void update();
+  bool isPressed() const;
+ bool isShortPress();
+  bool isLongPress();
 
 private:
-    uint8_t pin;
-    bool activeState;
-    unsigned long longPressDuration;
-    unsigned long debounceDelay;
-
-    bool shortPressDetected = false;
-    bool longPressDetected = false;
-    bool longPressJustStarted = false;
-    bool isPressing = false;
-    
-
-    unsigned long pressStartTime = 0;
-    unsigned long lastDebounceTime = 0;
-    bool lastReading = HIGH; // Default unpressed for pull-up config
+  uint8_t pin;
+  bool currentState = HIGH; // debounced reading
+  bool lastReading = HIGH;  // last raw reading
+  unsigned long lastDebounceTime = 0;
+  const unsigned long debounceDelay = 50; // adjust as needed
+  bool lastStableState;
+  bool currentStableState;
+  unsigned long pressStartTime;
+  bool longPressReported;
 };
--- src/ButtonHandler.cpp
+++ src/ButtonHandler.cpp
@@ -1,89 +1,123 @@
-#include "ButtonHandler.h"
-
-ButtonHandler::ButtonHandler(uint8_t pin, bool activeState, unsigned long longPress, unsigned long debounce)
-    : pin(pin), activeState(activeState), longPressDuration(longPress), debounceDelay(debounce) {
-    pinMode(pin, INPUT);
-}
-
-void ButtonHandler::reset() {
-    shortPressDetected = false;
-    longPressDetected = false;
-    longPressJustStarted = false;
-    Serial.println("ButtonHandler::reset() called");
-}
-
-void ButtonHandler::update() {
-    bool reading = digitalRead(pin);
 
 
-    if (reading != lastReading) {
-        lastDebounceTime = millis(); // reset debounce timer
-    }
+// ButtonHandler::ButtonHandler(uint8_t pin, bool activeState, unsigned long longPress, unsigned long debounce)
+//     : pin(pin), activeState(activeState), longPressDuration(longPress), debounceDelay(debounce) {
+//     pinMode(pin, INPUT);
+// }
 
-    if ((millis() - lastDebounceTime) > debounceDelay) {
-        bool pressed = (reading == activeState);
+// void ButtonHandler::reset() {
+//     shortPressDetected = false;
+//     longPressDetected = false;
+//     longPressJustStarted = false;
+//     Serial.println("ButtonHandler::reset() called");
+// }
 
-        if (pressed && !isPressing) {
-            // Button just pressed
-            isPressing = true;
-            pressStartTime = millis();
-            longPressDetected = false;
-            longPressJustStarted = false;
-        } else if (pressed && isPressing) {
-            // Button held
-            if (!longPressDetected && (millis() - pressStartTime >= longPressDuration)) {
-                longPressDetected = true;
-                longPressJustStarted = true; // <-- Flag moment of long press
-                Serial.println("Long press triggered");
-            }
-        } else if (!pressed && isPressing) {
-            // Button released
-            if (!longPressDetected) {
-                shortPressDetected = true;
-            }
-            isPressing = false;
-        }
-    }
+// void ButtonHandler::update() {
+//     bool reading = digitalRead(pin);
 
-    lastReading = reading;
-}
 
-bool ButtonHandler::isShortPress() {
-    if (shortPressDetected) {
-        shortPressDetected = false;
-        return true;
-    }
-    return false;
-}
+//     if (reading != lastReading) {
+//         lastDebounceTime = millis(); // reset debounce timer
+//     }
 
-// bool ButtonHandler::isLongPress() {
-//     if (longPressDetected && !isPressing) {
+//     if ((millis() - lastDebounceTime) > debounceDelay) {
+//         bool pressed = (reading == activeState);
+
+//         if (pressed && !isPressing) {
+//             // Button just pressed
+//             isPressing = true;
+//             pressStartTime = millis();
+//             longPressDetected = false;
+//             longPressJustStarted = false;
+//         } else if (pressed && isPressing) {
+//             // Button held
+//             if (!longPressDetected && (millis() - pressStartTime >= longPressDuration)) {
+//                 longPressDetected = true;
+//                 longPressJustStarted = true; // <-- Flag moment of long press
+//                 Serial.println("Long press triggered");
+//             }
+//         } else if (!pressed && isPressing) {
+//             // Button released
+//             if (!longPressDetected) {
+//                 shortPressDetected = true;
+//             }
+//             isPressing = false;
+//         }
+//     }
+
+//     lastReading = reading;
+// }
+
+// bool ButtonHandler::isShortPress() {
+//     if (shortPressDetected) {
+//         shortPressDetected = false;
+//         return true;
+//     }
+//     return false;
+// }
+
+
+
+// bool ButtonHandler::wasLongPressStart() {
+//     if (longPressDetected) {
 //         longPressDetected = false;
 //         return true;
 //     }
 //     return false;
 // }
 
-// bool ButtonHandler::isNewLongPress() {
-//     if (longPressJustStarted) {
-//         longPressJustStarted = false;
-//         return true;
-//     }
-//     return false;
+// bool ButtonHandler::isHeldLong() {
+//     return isPressing && (millis() - pressStartTime >= longPressDuration);
 // }
 
-bool ButtonHandler::wasLongPressStart() {
-    if (longPressDetected) {
-        longPressDetected = false;
-        return true;
-    }
-    return false;
+// bool ButtonHandler::isPhysicallyReleased() const {
+//     return lastReading != activeState;
+// }
+
+#include <Arduino.h>
+#include "ButtonHandler.h"
+
+ButtonHandler::ButtonHandler(uint8_t pin) : pin(pin) {
+    pinMode(pin, INPUT_PULLUP);  // enable internal pull-up
+
+    // Read initial state
+    bool initialState = digitalRead(pin);
+    currentStableState = initialState;
+    lastStableState = initialState;
+    currentState = initialState;  // Only if you’re using isPressed()
 }
 
-bool ButtonHandler::isHeldLong() {
-    return isPressing && (millis() - pressStartTime >= longPressDuration);
+void ButtonHandler::update() {
+  bool reading = digitalRead(pin);
+  unsigned long currentTime = millis();
+
+  if (reading != currentStableState) {
+      lastDebounceTime = currentTime;
+      currentStableState = reading;
+  }
+
+  if ((currentTime - lastDebounceTime) > debounceDelay) {
+      if (currentStableState != lastStableState) {
+          lastStableState = currentStableState;
+
+          if (currentStableState == LOW) { // Button pressed
+              Serial.println("Debounced State Changed: PRESSED");
+              pressStartTime = currentTime;
+              longPressReported = false;
+          } else { // Button released
+              Serial.println("Debounced State Changed: RELEASED");
+              if (!longPressReported && (currentTime - pressStartTime < 1000)) {
+                  Serial.println("Short press detected");
+              }
+          }
+      } else if (currentStableState == LOW && !longPressReported && (currentTime - pressStartTime >= 1000)) {
+          Serial.println("Long press detected");
+          longPressReported = true;
+      }
+  }
 }
 
-bool ButtonHandler::isPhysicallyReleased() const {
-    return lastReading != activeState;
+
+bool ButtonHandler::isPressed() const {
+    return currentState == LOW;
 }
